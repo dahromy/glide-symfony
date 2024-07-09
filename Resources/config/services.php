219e9fa5470644d5bc1e10150353a9ec50a24747
dahromy <romeorazaf@gmail.com>
2024-07-09 17:31:52 +0300

    Merge GlideExtensionRuntime into GlideExtension
    GlideExtensionRuntime was deleted and its functionality merged into GlideExtension. The apply glide filter and image URL generator methods are now part of GlideExtension. The services.php file was updated to reflect these changes.

--- Resources/config/services.php
+++ Resources/config/services.php
@@ -4,7 +4,6 @@
 use DahRomy\Glide\Controller\GlideController;
 use DahRomy\Glide\Service\GlideService;
 use DahRomy\Glide\Twig\Extension\GlideExtension;
-use DahRomy\Glide\Twig\Runtime\GlideExtensionRuntime;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
@@ -15,14 +14,12 @@ return static function (ContainerConfigurator $configurator): void {
         ->args(['%dahromy_glide.config%', service('request_stack'), '%dahromy_glide.signature_key%']);
 
     $services->set(GlideExtension::class)
-        ->tag('twig.extension');
-
-    $services->set(GlideExtensionRuntime::class)
         ->args([
             service('router'),
             service(GlideService::class),
             '%dahromy_glide.base_url%'
-        ]);
+        ])
+        ->tag('twig.extension');
 
     $services->set(GlideController::class)
         ->args([service(GlideService::class)])
--- src/Twig/Extension/GlideExtension.php
+++ src/Twig/Extension/GlideExtension.php
@@ -2,16 +2,60 @@
 
 namespace DahRomy\Glide\Twig\Extension;
 
-use DahRomy\Glide\Twig\Runtime\GlideExtensionRuntime;
+use DahRomy\Glide\Service\GlideService;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
 class GlideExtension extends AbstractExtension
 {
+    private UrlGeneratorInterface $router;
+    private GlideService $glideService;
+    private string $baseUrl;
+
+    public function __construct(
+        UrlGeneratorInterface $router,
+        GlideService $glideService,
+        string $baseUrl
+    ) {
+        $this->router = $router;
+        $this->glideService = $glideService;
+        $this->baseUrl = $baseUrl;
+    }
+
     public function getFilters(): array
     {
         return [
-            new TwigFilter('glide', [GlideExtensionRuntime::class, 'glideFilter'], ['is_safe' => ['html']]),
+            new TwigFilter('glide', [$this, 'glideFilter'], ['is_safe' => ['html']]),
         ];
+    }
+
+    /**
+     * Apply glide filter to the given path and return the generated image URL.
+     *
+     * @param string $path The path to the image.
+     * @param array $params An optional array of parameters to apply to the glide filter.
+     * @param string|null $preset An optional preset to apply to the glide filter.
+     * @return string The generated image URL.
+     */
+    public function glideFilter(string $path, array $params = [], string $preset = null): string
+    {
+        if ($preset) {
+            $params = array_merge($this->glideService->getPresets()[$preset] ?? [], $params);
+        }
+        $params['path'] = ltrim($path, '/');
+        return $this->generateImageUrl($params);
+    }
+
+    /**
+     * Generates an image URL based on given parameters.
+     *
+     * @param array $params The parameters for generating the URL.
+     *
+     * @return string The generated image URL.
+     */
+    private function generateImageUrl(array $params): string
+    {
+        return $this->baseUrl . $this->router->generate('dahromy_glide_asset', $params);
     }
 }
